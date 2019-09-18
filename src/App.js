e24674503f0dd13b2af515224e59c8f046ec9fda
toomusa <3895499+toomusa@users.noreply.github.com>
2019-09-18 14:57:45 -0700

    edit hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./containers/Home";
 import Projects from "./containers/Projects";
