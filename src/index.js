675f2229ee051b5cd128960133ef533a3e073d10
toomusa <3895499+toomusa@users.noreply.github.com>
2019-09-17 21:19:27 -0700

    redeploy

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 console.log('public url: ', process.env.PUBLIC_URL)
+console.log('public url: ', process.env.PUBLIC_URL)
 
 ReactDOM.render(<App />, document.getElementById('root'));
