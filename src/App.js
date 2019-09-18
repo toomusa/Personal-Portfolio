683ea381322f7d929afc6919224c9eaf3b3e4646
toomusa <3895499+toomusa@users.noreply.github.com>
2019-09-17 21:47:11 -0700

    removed basename

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <Router basename="/Personal-Portfolio/">
+      <Router>
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12 col-sm-12 col-xs-12" id="header">
