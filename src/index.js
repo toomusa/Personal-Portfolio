a177c5426378a8421808b544c0c3261e9d59a2cb
toomusa <3895499+toomusa@users.noreply.github.com>
2019-09-17 21:17:36 -0700

    add console log

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log('public url: ', process.env.PUBLIC_URL)
+
 ReactDOM.render(<App />, document.getElementById('root'));
